3840b7edf726af1aa5e50a1bcc4327f6f60e6c35
hidenShark <ilushashark@gmail.com>
2024-08-24 14:42:28 +0300

    Create project

---
+++ todo_List/todo_List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo_List
+//
+//  Created by Илья Акулов on 24.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
